1dea2182d0ef114f0b1bd8497d14d9ef1655eb3c
Atiqul-Isla-bit <71102223+Atiqul-Isla-bit@users.noreply.github.com>
2020-11-18 20:20:33 -0500

    Up to data

--- Kivy/PacemakerAppLogin.py
+++ Kivy/PacemakerAppLogin.py
@@ -5,6 +5,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.properties import ListProperty
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
@@ -18,8 +19,27 @@ from kivy.uix.button import Button
 from kivy.base import runTouchApp
 import os.path
 from kivy.clock import Clock
+import time
+import serial
 
 kv = Builder.load_file("pacemakerlogin.kv")
+
+## Serial Details
+
+#pacemaker_serial = serial.Serial(port="COM#", baudrate=...,timeout=...)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 ## Declare all the Screens ----------------------------------------------------------------------
@@ -33,7 +53,7 @@ class WelcomeWindow(Screen):
         manageWin.current = "loginWin"
     
     def goToReg(self):
-        if(len(userDatabase.users) < 10):
+        if(len(userDatabase.users) < 10):       ## there are less than 10 accounts
             manageWin.transition = SlideTransition()
             manageWin.transition.direction = "right"
             manageWin.current = "registerWin"
@@ -51,14 +71,14 @@ class LoginWindow(Screen):
 
 
     def btnLogin(self):
-        ## Check username and password for correctness
-
         user = self.nameField.text
         password = self.passwField.text
 
+        ## Check username and password for correctness
         ## if good...
         if(userDatabase.credentialCheck(user, password)):
             manageWin.transition = RiseInTransition()
+            manageWin.transition.duration = 0.15
             manageWin.current = "mainWin"
             MainWindow.currentUsername = user
             self.reset() ## clear the form
@@ -94,13 +114,15 @@ class RegisterWindow(Screen):
         username = self.username_Field.text
         password = self.password_Field.text
 
-        if (self.notEmpty(username, fname, lname, password)):  ##### add check for forbitten characters, ie: ";"
+        ## check for non empty strings and no forbitten characters, ie: ";"
+        if (self.notEmpty(username, fname, lname, password) and self.noBadChars(username, fname, lname, password)): 
             ## register user in database
             if(userDatabase.add_user(username, fname, lname, password) == 1):
                 ## user added succesfully
                 self.reset() ## clear the form
                 manageWin.transition = SlideTransition()
                 manageWin.transition.direction = "left"
+                manageWin.transition.duration = 0.15
                 manageWin.current = "welcomeWin"
                 registerComplete() # popup
             else:
@@ -114,6 +136,13 @@ class RegisterWindow(Screen):
         if(username.strip() != "" and firstName.strip() != "" and lastName.strip() != "" and password.strip() != ""):
             return 1
         else: return 0
+
+    def noBadChars(self,username,firstName,lastName,password):
+        badChars = [";","/"] ## all the illegal characters
+        for char in badChars:
+            if((char in username) or (char in firstName) or (char in lastName) or (char in password)):
+                return 0
+        else: return 1
 
 
     ## Go Back to Welcome
@@ -129,30 +158,70 @@ class RegisterWindow(Screen):
         self.username_Field.text = ""
         self.password_Field.text = ""
 
-## Mode Pop-up Window ----------------------------------------------
-class MyPopup(FloatLayout):
-    def closePopup(self):
-        popupWindow.dismiss()
 
 ## MainWindow ----------------------------------------
 
 class MainWindow(Screen):
     currentUser = ObjectProperty(None)
-    currentUsername = ""
+    display_active_pacingMode = ObjectProperty(None)
 
+    display_LRL_parameter = ObjectProperty(None)
+    display_URL_parameter = ObjectProperty(None)
+    display_AtrAmp_parameter = ObjectProperty(None)
+    display_VentAmp_parameter = ObjectProperty(None)
+    display_AtrPulseWidth_parameter = ObjectProperty(None)
+    display_VentPulseWidth_parameter = ObjectProperty(None)
+    display_VRP_parameter = ObjectProperty(None)
+    display_ARP_parameter = ObjectProperty(None)
+
+    #for later ### display_heartbeat_bpm = ObjectProperty(None) ## in progress
+
+
+    currentUsername = "" ## initialize the local variable, takes it's value from loginWindow btnLogin
+
+    ## Indicator for connected Hardware
+    indicatorColour = ListProperty([1,0,0,1]) ## defaults to red, becomes green if connected
     
+    global pacingMode, LRL, URL, AtrAmp, VentAmp, AtrPulseWidth, VentPulseWidth, VRP, ARP
+    pacingMode, LRL, URL, AtrAmp, VentAmp, AtrPulseWidth, VentPulseWidth, VRP, ARP = 9*["Not Set"]
+
+    global heartBPM #### in progress
+    global hardwareConnected
+    #change this for assignment 2
+    hardwareConnected = False ## set to board for assignment 2
+    heartBPM = 100 ## temporary
+
     def on_enter(self, *args):
-        self.currentUser.text = "Name: " + userDatabase.get_user(self.currentUsername)[0]
+        ##initialize the text labels
+        self.currentUser.text = "Active User: " + userDatabase.get_user(self.currentUsername)[0]
+        self.display_active_pacingMode.text = "Pacing Mode: " + pacingMode
+        self.display_LRL_parameter.text = "Lower Rate Limit: " + LRL
+        self.display_URL_parameter.text = "Upper Rate Limit: " + URL
+        self.display_AtrAmp_parameter.text = "Atrium Aplitude: " + AtrAmp
+        self.display_VentAmp_parameter.text = "Ventricle Amplitude: " + VentAmp
+        self.display_AtrPulseWidth_parameter.text = "AtrPulseWidth: " + AtrPulseWidth
+        self.display_VentPulseWidth_parameter.text = "VentPulseWidth: " + VentPulseWidth
+        self.display_ARP_parameter.text = "Atrium Refractory Period: " + ARP
+        self.display_VRP_parameter.text = "Ventricular Refractory Period: " + VRP
+        #self.display_heartbeat_bpm.text = "BPM: " + str(heartBPM) ####  for later 
+        
+
+
+        ## set hardware connected indicator
+        if(hardwareConnected):
+            self.indicatorColour = [0,1,0,1] ## green
+        else: 
+            self.indicatorColour = [1,0,0,1] ## defaults to red
 
     def logout(self):
         #edit transition
         manageWin.transition = FallOutTransition()
         manageWin.transition.duration = 0.15
         manageWin.current = "welcomeWin"
-        ## idea! popout: "Logout Successful"
+        ## popup "Logout Successful"
         signOut_Complete()
     
-    ## Option to delete account!!
+    ## Option to delete account
     def deleteAccount(self):
         userDatabase.remove_user(self.currentUsername)
         manageWin.transition = FallOutTransition()
@@ -161,57 +230,226 @@ class MainWindow(Screen):
         userDeleted()
     
     ## Mode Popup window is displayed
-    def open_popup(self):
-        show = MyPopup()
+    def open_modeSelector(self):
+        show = modeSelectorPopup()
         global popupWindow
         popupWindow = Popup(title="Pacemaker Modes", content=show,size_hint=(None,None), size=(500,500))
         popupWindow.open()
+
+    ## Programmable Parameters Popup window is displayed
+    def open_programmableParameters(self):
+        show = programmableParametersPopup()
+        global popupWindow
+        popupWindow = Popup(title="Programmable Parameters", content=show,size_hint=(None,None), size=(500,500))
+        popupWindow.open()
+    
+    ## Saves the parameter data into the user_data.txt file to deploy in the future
+    def deploy(self):
+        self.file = open("user_data.txt", "w")
+        self.data = {}
+        self.file.write( self.currentUsername + ";" + URL + ";" + LRL + ";" + AtrAmp + ";" + VentAmp + ";" + AtrPulseWidth + ";" + VentPulseWidth + ";" + VRP + ";" + ARP + "\n")
+        self.file.close()
+
     
 
-## WindowManager ----------------------------------------
+## Declare all Popups Layout Classes ----------------------------------------------------------------------
 
-class WindowManager(ScreenManager):
-    pass
+## Main page popups
+class modeSelectorPopup(FloatLayout):
+    def closePopup(self):
+        popupWindow.dismiss()
 
-manageWin = WindowManager(transition=SlideTransition())
+    def setPacingMode(self,mode):
+        setPacingModetext(mode)
 
-## Add all the pages
-manageWin.add_widget(WelcomeWindow(name="welcomeWin"))
-manageWin.add_widget(LoginWindow(name="loginWin"))
-manageWin.add_widget(RegisterWindow(name="registerWin"))
-manageWin.add_widget(MainWindow(name="mainWin"))
+# Popup for programmableParameterPopup
+class programmableParametersPopup(FloatLayout):
+    
+    # Store the index variable to keep track of which parameter to be changed
+    def setIndex(self, num):
+        global index
+        index = num
 
-manageWin.current = "welcomeWin"
+    # Open popup for text input
+    def open_textInput(self, title):
+        show = textInputPopup()
+        global popupWindow_editParameter
+        popupWindow_editParameter = Popup(title=title, content=show,size_hint=(None,None), size=(350,300))
+        popupWindow_editParameter.open()
+
+    def closePopup(self):
+        popupWindow.dismiss()
+        manageWin.transition = NoTransition()
+        manageWin.current = "welcomeWin"
+        manageWin.current = "mainWin"
+
+# Popup for text input
+class textInputPopup(FloatLayout):
+
+    inputField = ObjectProperty(None)
+
+    # Check which parameter is being changed and set it to the text inputted
+    def selectProgParam(self):
+        num = self.inputField.text
+
+        if index == 1:
+            if int(num) > 150 or int(num) < 40:
+                show = errorPopup()
+                global popupWindow
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                setLRL(num)
+
+        elif index == 2:
+            if int(num) > 150 or int(num) < 60:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                setURL(num)
+
+        elif index == 3:
+            if int(num) > 5 or int(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                setAtrAmp(num)
+
+        elif index == 4:
+            if float(num) > 100 or float(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                setAtrPulseWidth(num)
+
+        elif index == 5:
+            if float(num) > 600 or float(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                setARP(num)
+
+        elif index == 6:
+            if int(num) > 5 or int(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                 setVentAmp(num)
+
+        elif index == 7:
+            if float(num) > 100 or float(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                  setVentPulseWidth(num)
+
+        elif index == 8:
+            if float(num) > 600 or float(num) < 0:
+                show = errorPopup()
+                popupWindow = Popup(title="Input Error", content=show,size_hint=(None,None), size=(300,200))
+                popupWindow.open()
+            else:
+                  setVRP(num)
+
+    def closePopup(self):
+        popupWindow_editParameter.dismiss()
+        manageWin.transition = NoTransition()
+        manageWin.current = "welcomeWin"
+        manageWin.current = "mainWin"
 
 
-
-## Error Popup stuff ----------------------------------------------------------------------
-
+## Generic Errors
 class errorPopup(FloatLayout):
-    ## The following isn't working!
-    #dialogueMessage = ObjectProperty(None)
-    # def setMessage(self,message):
-    #     self.dialogueMessage.text = "Hellow"
-    
     def closePopup(self):
         popupWindow.dismiss()
 
-
-#maximum accounts reached 
-class errorMaxPopup(FloatLayout):    
+## maximum accounts reached 
+class errorMaxPopup(FloatLayout):
     def closePopup(self):
         popupWindow.dismiss()
 
-
+## Auto-timout Popup
 class successPopup(FloatLayout):
     def __init__(self, **kwargs):
         super(successPopup, self).__init__(**kwargs)
-        # call dismiss_popup in 1 second
+        # call dismiss_popup after 1 second
         Clock.schedule_once(self.closePopup, 1)
 
     def closePopup(self, timer):
         popupWindow.dismiss()
 
+
+
+
+## Global Vars and Functions ----------------------------------------------------------------------
+
+
+def setPacingModetext(mode):
+    global pacingMode
+    pacingMode = mode
+    print(pacingMode)
+    manageWin.transition = NoTransition()
+    manageWin.current = "welcomeWin"
+    manageWin.current = "mainWin"
+
+    ##testing, for demo purpose only. remove for assignment 2
+    global hardwareConnected
+    hardwareConnected = True
+    ## testing end
+
+
+## Set programmable parameters
+def setLRL(num):
+    global LRL  ## double
+    LRL = num + " BPM"   ##bpm rate between roughly 30 and 100
+    print("LRL: " + LRL)
+
+def setURL(num):
+    global URL  ## double
+    URL = num + " BPM"   ##bpm rate between roughly 80 and 150
+    print("URL: " + URL)
+
+def setAtrAmp(num):
+    global AtrAmp
+    AtrAmp = num + " V"   ##voltage between 0 and 5V
+    print("AtrAmp: " + AtrAmp)
+    
+def setAtrPulseWidth(num):
+    global AtrPulseWidth
+    AtrPulseWidth = num + " ms"      ##time between ~1 to 30 msec
+    print("AtrPulseWidth: " + AtrPulseWidth)
+    
+def setVentAmp(num):
+    global VentAmp
+    VentAmp = num + " V"   ##voltage between 0 and 5V
+    print("VentAmp: " + VentAmp)
+    
+def setVentPulseWidth(num):
+    global VentPulseWidth
+    VentPulseWidth = num + " ms"    ##time between ~1 to 30 msec
+    print("VentPulseWidth: " + VentPulseWidth)
+    
+def setVRP(num):
+    global VRP
+    VRP = num + " ms"           ##time between ~1 to 500 msec
+    print("VRP: " + VRP)
+    
+def setARP(num):
+    global ARP
+    ARP = num + " ms"          ##time between ~1 to 500 msec
+    print("ARP: " + ARP)
+
+
+
+
+
+## Initialize the Popups ------------------------------------------------------
 
 def invalidLogin():
     show = errorPopup()
@@ -238,8 +476,6 @@ def signOut_Complete():
     popupWindow.open()
 
 def accountLimitReached():
-    #errorPopup().setMessage(message = "Account Limit Reached. Only 10 accounts can be stored locally at once.")
-    #print(errorPopup().dialogueMessage.text)
     show = errorMaxPopup()
     global popupWindow
     popupWindow = Popup(title="Error", content=show,size_hint=(None,None), size=(300,200))
@@ -253,6 +489,25 @@ def userDeleted():
 
 
 
+
+## WindowManager --------------------------------------------
+
+class WindowManager(ScreenManager):
+    pass
+
+manageWin = WindowManager(transition=SlideTransition())
+
+## Add all the pages ------------------------------------------
+manageWin.add_widget(WelcomeWindow(name="welcomeWin"))
+manageWin.add_widget(LoginWindow(name="loginWin"))
+manageWin.add_widget(RegisterWindow(name="registerWin"))
+manageWin.add_widget(MainWindow(name="mainWin"))
+
+manageWin.current = "welcomeWin"
+
+
+## Database Details ----------------------------------------------------------------------
+
 #check if file exists
 if(os.path.isfile("saved_users.txt")):
     print("file located successfuly")
@@ -261,8 +516,18 @@ else:
     f.close()
     print("no file found, new file was created")
 
+if(os.path.isfile("user_data.txt")):
+    print("file located successfuly")
+else:
+    f = open("user_data.txt", "w")
+    f.close()
+    print("no file found, new file was created")
+
 ## Load the database
 userDatabase = Database("saved_users.txt")
+
+## If a new database class needs to be created 
+#parameterDatabase = paramDatabase("user_data.txt")
 
 
 ## Run the App ----------------------------------------------------------------------
